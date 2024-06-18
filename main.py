ef3e75faec30bc7b85a4bd06ce6556ccc35d623c
ni-eminen <matiasnie2@gmail.com>
2024-06-18 14:42:45 +0300

    operator profit modeling ready

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from scipy.stats import gamma, binom
 import itertools
 from scipy.stats import gaussian_kde
+import time
 
 
 class SalesOperator:
@@ -26,7 +27,8 @@ class SalesOperator:
         return deals, sales
 
     def convolution(self, quantities, operation):
-        z = itertools.product(quantities)
+        a, b = quantities
+        z = itertools.product(a, b)
         if operation == '+':
             z = [a + b for (a, b) in z]
         elif operation == '-':
@@ -34,30 +36,41 @@ class SalesOperator:
         elif operation == '*':
             z = [a * b for (a, b) in z]
 
-        return z
+        return np.array(z)
 
     def model_pdf(self, model_distribution):
-        model_density = gaussian_kde((np.array(model_distribution) - 3000) / 2)
+        model_density = gaussian_kde(model_distribution)
         model_values = np.linspace(min(model_distribution), max(model_distribution), 1000)
         model_pdf = model_density(model_values)
 
         return model_density, model_values, model_pdf
 
-    def cdf_quantity(self, amt, model_density, quantity):
+    def quantity_cdf(self, amt, model_density, quantity):
         probability = np.trapz(y=model_density(np.linspace(amt, max(np.array(quantity)), 1000)),
                                x=np.linspace(amt, max(np.array(quantity)), 1000))
 
         return probability
 
-    def p_profit(self, amt):
-        model_density, model_values, model_pdf = self.model_pdf(self.convolution([self.deals, self.sales], '*'))
-        probability = self.cdf_quantity()
+    def p_profit(self, amt, distribution):
+        model_density, model_values, model_pdf = self.model_pdf((np.array(distribution) - 3000) / 2)
+        probability = self.quantity_cdf(amt, model_density, distribution)
+
+        return probability
 
 def plot_sample(sample, kind='kde'):
     sns.displot(sample, kind=kind)
 
     plt.show()
 
+def time_f(f, t='f'):
+    start = time.time()
+    x = f()
+    end = time.time()
+    print(t, end - start)
+    return x
 
-operator = SalesOperator(sales=gamma, deals=binom)
-p_profit = operator.p_profit(3000)
+operator = time_f(lambda: SalesOperator(sales=gamma, deals=binom), 'creating operator')
+income = time_f(lambda: operator.convolution((operator.deals_samples, operator.sales_samples), '*'), 'creating income')
+
+p_profit = time_f(lambda: operator.p_profit(3000, (income - 3000) / 2), 'creating p_profit')
+print(p_profit)
